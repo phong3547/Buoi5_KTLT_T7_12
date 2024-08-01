543053a606e5b690f438723cc0d5487c10a85eb3
Phong Đặng Văn <phongdang6889@gmail.com>
2024-08-01 15:36:32 +0700

    Bai3_Buoi5

---
+++ Bài3.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+// Hàm tìm giá trị phần tử thứ n của cấp số cộng
+int capSoCong(int a, int r, int n) {
+    if (n == 1) {
+        return a;
+    }
+    else {
+        return capSoCong(a, r, n - 1) + r;
+    }
+}
+
+int main() {
+    int a, r, n;
+    printf("Nhap hang dau a: ");
+    scanf_s("%d", &a);
+    printf("Nhap cong sai r: ");
+    scanf_s("%d", &r);
+    printf("Nhap chi so n: ");
+    scanf_s("%d", &n);
+
+    int un = capSoCong(a, r, n);
+    printf("Gia tri phan tu thu %d cua cap so cong la: %d\n", n, un);
+
+    return 0;
+}

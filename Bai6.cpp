2eb91a3717944cb7369cc4278412d523aae2cf19
Phong Đặng Văn <phongdang6889@gmail.com>
2024-08-03 19:50:09 +0700

    Bai6

---
+++ Bai6.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+
+// Hàm đệ quy để đếm số chữ số của số nguyên dương n
+int countDigitsRecursive(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    return 1 + countDigitsRecursive(n / 10);
+}
+
+int main() {
+    int n;
+    printf("Nhap so nguyen duong n: ");
+    scanf_s("%d", &n);
+
+    if (n <= 0) {
+        printf("Vui long nhap mot so nguyen duong.\n");
+    }
+    else {
+        // Trường hợp đặc biệt khi n = 0
+        if (n == 0) {
+            printf("So chu so cua %d la: 1\n", n);
+        }
+        else {
+            printf("So chu so cua %d la: %d\n", n, countDigitsRecursive(n));
+        }
+    }
+
+    return 0;
+}
